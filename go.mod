44f186acee4a0dbe1280c56353f9abe6811788e0
arynskiii <muratov_aryn@mail.ru>
2023-10-17 09:40:45 +0600

    last

--- go.mod
+++ go.mod
@@ -3,20 +3,22 @@ module doodocs_task
 go 1.20
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.2
 	github.com/gin-gonic/gin v1.9.1
 	github.com/golang/mock v1.4.4
 	github.com/joho/godotenv v1.5.1
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.17.0
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/bytedance/sonic v1.10.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -32,6 +34,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
--- internal/delivery/email_test.go
+++ internal/delivery/email_test.go
@@ -1,44 +1,78 @@
 package handler
 
 import (
-	"doodocs_task/internal/service"
-	mock_service "doodocs_task/internal/service/mock"
-	"github.com/gin-gonic/gin"
-	"github.com/golang/mock/gomock"
+	"archive/zip"
+	"bytes"
+	"fmt"
+	"io"
+	"math/rand"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"net/textproto"
 	"testing"
 )
 
-func TestHandler_sendfile(t *testing.T) {
-	type mockBehaviour func(s *mock_service.MockEmail)
+func createRandomZipContent() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	zipWriter := zip.NewWriter(buf)
 
-	type fields struct {
-		service *service.Service
+	// Generate random content and add it to the zip archive
+	for i := 0; i < 5; i++ {
+		fileName := fmt.Sprintf("file%d.txt", i)
+		content := make([]byte, 100) // Generate random content
+		rand.Read(content)
+		fileWriter, err := zipWriter.Create(fileName)
+		if err != nil {
+			return nil, err
+		}
+		_, err = fileWriter.Write(content)
+		if err != nil {
+			return nil, err
+		}
 	}
-	type args struct {
-		c *gin.Context
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
 
-			c := gomock.NewController(t)
-			defer c.Finish()
+	zipWriter.Close()
+	return buf.Bytes(), nil
+}
 
-			emailService := mock_service.NewMockEmail(c)
-			testService := &service.Service{
-				Email: emailService,
-			}
-			h := &Handler{
-				service: testService,
-			}
+func TestCreateArchiveHandler(t *testing.T) {
 
-			h.sendfile(tt.args.c)
-		})
-	}
+	t.Run("Successfully create archive", func(t *testing.T) {
+		randomZipContent, err := createRandomZipContent()
+		if err != nil {
+			t.Fatalf("Failed to create random zip content: %v", err)
+		}
+
+		var b bytes.Buffer
+		writer := multipart.NewWriter(&b)
+
+		h := make(textproto.MIMEHeader)
+		h.Set("Content-Disposition", `form-data; name="files[]"; filename="random.zip"`)
+		h.Set("Content-Type", "application/zip")
+
+		part, err := writer.CreatePart(h)
+		if err != nil {
+			t.Fatalf("Failed to create multipart part: %v", err)
+		}
+
+		_, err = io.Copy(part, bytes.NewReader(randomZipContent))
+		if err != nil {
+			t.Fatalf("Failed to copy random zip content: %v", err)
+		}
+
+		writer.Close()
+
+		req := httptest.NewRequest(http.MethodPost, "http://example.com/archive", &b)
+		req.Header.Set("Content-Type", writer.FormDataContentType())
+		rr := httptest.NewRecorder()
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		if rr.Header().Get("Content-Type") != "application/zip" {
+			t.Errorf("handler returned wrong content type: got %v want %v", rr.Header().Get("Content-Type"), "application/zip")
+		}
+	})
 }
